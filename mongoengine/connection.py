exfm/mongoengine

commit 1781c4638b0bc7b510c6c3cea27305fcb933941e

Changed how the connection identity key is made
Uses the current thread identity as well as the process idenity to form
the key.

Fixes #151

mongoengine/connection.py
@@ -1,5 +1,6 @@
 from pymongo import Connection
 import multiprocessing
+import threading
 
 __all__ = ['ConnectionError', 'connect']
 
@@ -22,6 +23,8 @@ class ConnectionError(Exception):
 
 
 def _get_connection(reconnect=False):
+    """Handles the connection to the database
+    """
     global _connection
     identity = get_identity()
     # Connect to the database if not already connected
@@ -33,6 +36,9 @@ def _get_connection(reconnect=False):
     return _connection[identity]
 
 def _get_db(reconnect=False):
+    """Handles database connections and authentication based on the current
+    identity
+    """
     global _db, _connection
     identity = get_identity()
     # Connect if not already connected
@@ -52,12 +58,17 @@ def _get_db(reconnect=False):
     return _db[identity]
 
 def get_identity():
+    """Creates an identity key based on the current process and thread
+    identity.
+    """
     identity = multiprocessing.current_process()._identity
     identity = 0 if not identity else identity[0]
+
+    identity = (identity, threading.current_thread().ident)
     return identity
-    
+
 def connect(db, username=None, password=None, **kwargs):
-    """Connect to the database specified by the 'db' argument. Connection 
+    """Connect to the database specified by the 'db' argument. Connection
     settings may be provided here as well if the database is not running on
     the default port on localhost. If authentication is needed, provide
     username and password arguments as well.

commit fd061d6355ed2f09530827ecc1bb5a72040dc60a

Got back to old connection module to fix regression introduced by 1781c46.  Leave in the new version cause i'll merge them together someday

mongoengine/connection.py
@@ -1,71 +1,55 @@
 from pymongo import Connection
-import multiprocessing
-import threading
+
 
 __all__ = ['ConnectionError', 'connect']
 
 
-_connection_defaults = {
+_connection_settings = {
     'host': 'localhost',
     'port': 27017,
+    'pool_size': 1,
 }
-_connection = {}
-_connection_settings = _connection_defaults.copy()
+_connection = None
 
 _db_name = None
 _db_username = None
 _db_password = None
-_db = {}
+_db = None
 
 
 class ConnectionError(Exception):
     pass
 
 
-def _get_connection(reconnect=False):
-    """Handles the connection to the database
-    """
+def _get_connection():
     global _connection
-    identity = get_identity()
     # Connect to the database if not already connected
-    if _connection.get(identity) is None or reconnect:
+    if _connection is None:
         try:
-            _connection[identity] = Connection(**_connection_settings)
-        except Exception, e:
-            raise ConnectionError("Cannot connect to the database:\n%s" % e)
-    return _connection[identity]
+            _connection = Connection(**_connection_settings)
+        except:
+            raise ConnectionError('Cannot connect to the database')
+    return _connection
 
-def _get_db(reconnect=False):
-    """Handles database connections and authentication based on the current
-    identity
-    """
+
+def _get_db():
     global _db, _connection
-    identity = get_identity()
     # Connect if not already connected
-    if _connection.get(identity) is None or reconnect:
-        _connection[identity] = _get_connection(reconnect=reconnect)
+    if _connection is None:
+        _connection = _get_connection()
 
-    if _db.get(identity) is None or reconnect:
+    if _db is None:
         # _db_name will be None if the user hasn't called connect()
         if _db_name is None:
             raise ConnectionError('Not connected to the database')
 
         # Get DB from current connection and authenticate if necessary
-        _db[identity] = _connection[identity][_db_name]
+        _db = _connection[_db_name]
         if _db_username and _db_password:
-            _db[identity].authenticate(_db_username, _db_password)
+            _db.authenticate(_db_username, _db_password)
 
-    return _db[identity]
+    return _db
 
-def get_identity():
-    """Creates an identity key based on the current process and thread
-    identity.
-    """
-    identity = multiprocessing.current_process()._identity
-    identity = 0 if not identity else identity[0]
-
-    identity = (identity, threading.current_thread().ident)
-    return identity
 
 def connect(db, username=None, password=None, **kwargs):
     """Connect to the database specified by the 'db' argument. Connection
@@ -73,10 +57,98 @@ def connect(db, username=None, password=None, **kwargs):
     the default port on localhost. If authentication is needed, provide
     username and password arguments as well.
     """
-    global _connection_settings, _db_name, _db_username, _db_password, _db
-    _connection_settings = dict(_connection_defaults, **kwargs)
+    global _connection_settings, _db_name, _db_username, _db_password
+    _connection_settings.update(kwargs)
     _db_name = db
     _db_username = username
     _db_password = password
-    return _get_db(reconnect=True)
+    return _get_db()
 
+
+# from pymongo import Connection
+# import multiprocessing
+# import threading
+
+# __all__ = ['ConnectionError', 'connect', 'use_identity']
+
+
+# _connection_defaults = {
+#     'host': 'localhost',
+#     'port': 27017,
+# }
+# _connection = {}
+# _connection_settings = _connection_defaults.copy()
+
+# _db_name = None
+# _db_username = None
+# _db_password = None
+# _db = {}
+
+# use_identity = True
+
+
+# class ConnectionError(Exception):
+#     pass
+
+
+# def _get_connection(reconnect=False):
+#     """Handles the connection to the database
+#     """
+#     global _connection
+#     if use_identity:
+#         identity = get_identity()
+#         # Connect to the database if not already connected
+#         if _connection.get(identity) is None or reconnect:
+#             try:
+#                 _connection[identity] = Connection(**_connection_settings)
+#             except Exception, e:
+#                 raise ConnectionError("Cannot connect to the database:\n%s" % e)
+#         return _connection[identity]
+
+
+# def _get_db(reconnect=False):
+#     """Handles database connections and authentication based on the current
+#     identity
+#     """
+#     global _db, _connection
+#     identity = get_identity()
+#     # Connect if not already connected
+#     if _connection.get(identity) is None or reconnect:
+#         _connection[identity] = _get_connection(reconnect=reconnect)
+
+#     if _db.get(identity) is None or reconnect:
+#         # _db_name will be None if the user hasn't called connect()
+#         if _db_name is None:
+#             raise ConnectionError('Not connected to the database')
+
+#         # Get DB from current connection and authenticate if necessary
+#         _db[identity] = _connection[identity][_db_name]
+#         if _db_username and _db_password:
+#             _db[identity].authenticate(_db_username, _db_password)
+
+#     return _db[identity]
+
+
+# def get_identity():
+#     """Creates an identity key based on the current process and thread
+#     identity.
+#     """
+#     identity = multiprocessing.current_process()._identity
+#     identity = 0 if not identity else identity[0]
+
+#     identity = (identity, threading.current_thread().ident)
+#     return identity
+
+
+# def connect(db, username=None, password=None, **kwargs):
+#     """Connect to the database specified by the 'db' argument. Connection
+#     settings may be provided here as well if the database is not running on
+#     the default port on localhost. If authentication is needed, provide
+#     username and password arguments as well.
+#     """
+#     global _connection_settings, _db_name, _db_username, _db_password, _db
+#     _connection_settings = dict(_connection_defaults, **kwargs)
+#     _db_name = db
+#     _db_username = username
+#     _db_password = password
+#     return _get_db(reconnect=True)